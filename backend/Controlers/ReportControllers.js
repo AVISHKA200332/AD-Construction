6f98bb9f64885a345f293caf80831adc2aa2d203
bhanuka598 <bhanuka_pawan@yahoo.com>
2025-08-18 22:03:12 +0530

    Completed retrieve data by id

--- backend/Controlers/ReportControllers.js
+++ backend/Controlers/ReportControllers.js
@@ -3,21 +3,21 @@ const Report = require("../Model/ReportModel");
 // display data
 const getAllReports = async (req, res, next) => {
     
-    let Reports;
+    let report;
     // Get All Reports
     try{
-        reports = await Report.find();
+        report = await Report.find();
     }catch (err){
         console.log(err);
     }
 
     // not found reports
-    if(!reports){
+    if(!report){
         return res.status(404).json({message:"Report not found"});
     }
 
     // display all reports
-    return res.status(200).json({ reports });
+    return res.status(200).json({ report });
 };
 
 // insert data
@@ -25,21 +25,42 @@ const addReports = async (req, res, next) => {
 
     const {project_Id,record_Type,party_Name,amount,date,status,description,bank_Slip_Ref_No} = req.body;
 
-    let reports;
+    let report;
 
     try {
-        reports = new Report({project_Id,record_Type,party_Name,amount,date,status,description,bank_Slip_Ref_No});
-        await reports.save();
+        report = new Report({project_Id,record_Type,party_Name,amount,date,status,description,bank_Slip_Ref_No});
+        await report.save();
     }catch (err) {
         console.log(err);
     }
 
     // if not insert reports
-    if (!reports){
+    if (!report){
         return res.status(404).json({message:"Unable to add reports"});
     }
-    return res.status(200).json({reports});
+    return res.status(200).json({report});
 };
+
+//get by id
+const getById = async (req, res, next) => {
+
+    const report_Id = req.params.id;
+
+    let report;
+
+    try {
+        report = await Report.findById(report_Id);
+    }catch (err) {
+        console.log(err);
+    }
+
+    // not available reports
+    if(!report){
+        return res.status(404).json({message:"Report not found"});
+    }
+    return res.status(200).json({report});
+}
 
 exports.getAllReports = getAllReports;
 exports.addReports = addReports;
+exports.getById = getById;
--- backend/Routes/ReportRoutes.js
+++ backend/Routes/ReportRoutes.js
@@ -7,6 +7,7 @@ const ReportController = require("../Controlers/ReportControllers");
 
 router.get("/",ReportController.getAllReports);
 router.post("/",ReportController.addReports);
+router.get("/:id",ReportController.getById);
 
 //export
 module.exports = router;
