af504ca1af361c1cbd0f79b8e3444e0309771b50
Bhanuka Pawan Athukorala <bhanuka_pawan@yahoo.com>
2025-09-03 07:32:48 +0530

    Updated routes for financial backend app.js

--- Backend/app.js
+++ Backend/app.js
@@ -3,12 +3,14 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const router = require('./Route/UserRoute');
+const report_router = require("./Routes/ReportRoutes");
 
 const app = express();
 app.use(express.json()); // always include this for JSON body parsing
 
 // Middleware
 app.use("/users", router);
+app.use("/reports", report_router);
 
 mongoose.connect("mongodb+srv://Admin:1jRinOK59GDesfiB@cluster0.pmkuy4i.mongodb.net/")
 .then(() => console.log("Connected to MongoDB"))
