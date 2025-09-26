4ea239e94814e3655ba669a40c415dbac497a37b
Bhanuka Pawan Athukorala <bhanuka_pawan@yahoo.com>
2025-09-26 14:58:58 +0530

    Add Backend

---
+++ Backend/app.js
@@ -0,0 +1,22 @@
+//pass: 3KzKmzJ6QFuLjCBA
+
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./Routes/FinanceRoutes");
+const projectRouter = require("./Route/ProjectRoute");
+
+const app = express();
+const cors = require("cors");
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+app.use("/projects", projectRouter);
+app.use("/finance", router);
+
+mongoose.connect("mongodb+srv://Admin:1jRinOK59GDesfiB@cluster0.pmkuy4i.mongodb.net/")
+.then(()=> console.log("Connected to MongoDB"))
+.then(() => {
+    app.listen(5000);
+})
+.catch((err)=> console.log((err)));
