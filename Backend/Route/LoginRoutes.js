e619f54f1139ddffdb7abd3df850a83f5b66d5f4
AVISHKA200332 <it23615502@my.sliit.lk>
2025-09-26 09:51:05 +0530

    Delete Backend directory

--- Backend/Route/LoginRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const loginController = require('../Controllers/LoginController');
-
-// Login route
-router.post('/', loginController.login);
-
-module.exports = router;
