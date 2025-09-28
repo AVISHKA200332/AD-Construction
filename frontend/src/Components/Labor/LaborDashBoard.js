52bd35dc1177c04ba25c4f77ff0873103607795a
AVISHKA200332 <it23615502@my.sliit.lk>
2025-09-28 13:19:18 +0530

    Revert "Merge branch 'Tharuki's-Branch' into Dev"
    This reverts commit fbb0d6fe31f0b09802b48c201683353a86f40784, reversing
    changes made to 7d3ca168038bb0f44813aae28e17ab405aff1c8f.

---
+++ frontend/src/Components/Labor/LaborDashBoard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LaborDashboard = () => {
+  return (
+    <div style={{ padding: "1rem" }}>
+      <h1>Labor Dashboard</h1>
+      <p>Welcome to the Labor Dashboard.</p>
+    </div>
+  );
+};
+
+export default LaborDashboard;
